009db39c4b657cbde29c904e5388e4912e900c03
RussellSchool <rschool@autoreg-163143.dyn.wpi.edu>
2020-01-28 16:31:35 -0500

    Creating merge conflict

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String [ ] args)
     {
         ExJessieJames jessieJames = new ExJessieJames();
-        System.out.println(jessieJames.jessieJamesMethod());
+        System.out.println("do this instead");
     }
 }
